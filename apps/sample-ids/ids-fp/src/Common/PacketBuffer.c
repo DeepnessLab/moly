e0f5a7650d229b1096bf82e2c277191b0d3bdc40
Yotam Harchol <yotamhc@gmail.com>
2014-06-11 08:22:04 +0300

    Changed reporting mechanism to send reports in a separate packet.

---
+++ apps/sample-ids/ids-fp/src/Common/PacketBuffer.c
@@ -0,0 +1,126 @@
+/*
+ * PacketBuffer.c
+ *
+ *  Created on: Jun 8, 2014
+ *      Author: yotamhc
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include "PacketBuffer.h"
+
+static struct timespec _100_nanos = {0, 100};
+
+static inline void lock(PacketBuffer *q) {
+	while (__sync_lock_test_and_set(&(q->lock), 1)) {
+		nanosleep(&_100_nanos, NULL);
+	}
+}
+
+static inline void unlock(PacketBuffer *q) {
+	__sync_lock_release(&(q->lock));
+}
+
+void packet_buffer_init(PacketBuffer *q) {
+	q->size = 0;
+	q->head = q->tail = NULL;
+}
+
+void packet_buffer_destroy(PacketBuffer *q, int destroyItems) {
+	PacketBufferItem *item, *next;
+	item = q->head;
+	while (item) {
+		if (destroyItems) {
+			free(item->packet);
+		}
+		next = item->next;
+		free(item);
+		item = next;
+	}
+	q->head = q->tail = NULL;
+	q->size = 0;
+}
+
+void packet_buffer_enqueue(PacketBuffer *q, InPacket *packet) {
+	PacketBufferItem *i;
+
+	lock(q);
+
+	i = (PacketBufferItem*)malloc(sizeof(PacketBufferItem));
+	i->packet = packet;
+	i->next = NULL;
+	i->prev = q->tail;
+	if (q->tail)
+		q->tail->next = i;
+	q->tail = i;
+	if (!q->head)
+		q->head = i;
+	q->size++;
+
+	unlock(q);
+}
+
+InPacket *packet_buffer_dequeue(PacketBuffer *q) {
+	void *res;
+	PacketBufferItem *i;
+
+	if (q->size == 0)
+		return NULL;
+
+	lock(q);
+
+	i = q->head;
+	res = i->packet;
+	q->head = i->next;
+	q->head->prev = NULL;
+	if (q->size == 1)
+		q->tail = NULL;
+	q->size--;
+	free(i);
+
+	unlock(q);
+
+	return res;
+}
+
+InPacket *packet_buffer_peek(PacketBuffer *q) {
+	if (q->size == 0)
+		return NULL;
+
+	return q->head->packet;
+}
+
+InPacket *packet_buffer_pop(PacketBuffer *q, unsigned int src_ip, unsigned int dst_ip, unsigned short src_port, unsigned short dst_port, unsigned int seqnum) {
+	PacketBufferItem *i, *temp;
+	InPacket *res;
+
+	if (q->size == 0)
+		return NULL;
+
+	lock(q);
+
+	i = q->head;
+	while (i) {
+		if (i->packet->seqnum == seqnum &&
+				i->packet->src_ip == src_ip &&
+				i->packet->dst_ip == dst_ip &&
+				i->packet->src_port == src_port &&
+				i->packet->dst_port == dst_port) {
+			res = i->packet;
+
+			// Remove element
+			temp = i->prev;
+			if (temp)
+				temp->next = i->next;
+			if (i->next)
+				i->next->prev = temp;
+
+			unlock(q);
+			return res;
+		}
+		i = i->next;
+	}
+
+	unlock(q);
+	return NULL;
+}
---
+++ apps/sample-ids/ids-fp/src/Common/PacketBuffer.h
@@ -0,0 +1,49 @@
+/*
+ * PacketBuffer.h
+ *
+ *  Created on: Jun 8, 2014
+ *      Author: yotamhc
+ */
+
+#ifndef PACKETBUFFER_H_
+#define PACKETBUFFER_H_
+
+#include <pcap.h>
+
+typedef struct {
+	struct pcap_pkthdr pkthdr;
+	unsigned char *pktdata;
+	unsigned int payload_len;
+	unsigned long timestamp;
+	unsigned int seqnum;
+	unsigned int src_ip;
+	unsigned int dst_ip;
+	unsigned short src_port;
+	unsigned short dst_port;
+	unsigned short proto;
+} InPacket;
+
+typedef struct sq_item {
+	InPacket *packet;
+	struct sq_item *next, *prev;
+} PacketBufferItem;
+
+typedef struct {
+	PacketBufferItem *head, *tail;
+	int size;
+	int lock;
+} PacketBuffer;
+
+void packet_buffer_init(PacketBuffer *q);
+
+void packet_buffer_destroy(PacketBuffer *q, int destroyItems);
+
+void packet_buffer_enqueue(PacketBuffer *q, InPacket *packet);
+
+InPacket *packet_buffer_dequeue(PacketBuffer *q);
+
+InPacket *packet_buffer_peek(PacketBuffer *q);
+
+InPacket *packet_buffer_pop(PacketBuffer *q, unsigned int src_ip, unsigned int dst_ip, unsigned short src_port, unsigned short dst_port, unsigned int seqnum);
+
+#endif /* PACKETBUFFER_H_ */
